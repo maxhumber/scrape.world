0a432c9c641b1a5c54d86aed4b79a068056e999c
Max Humber <max.humber@gmail.com>
2020-02-17 15:17:17 -0500

    edits

---
+++ scrape/soup.py
@@ -0,0 +1,17 @@
+from gazpacho import get, Soup
+
+url = 'http://www.scrape.world/soup'
+html = get(url)
+soup = Soup(html)
+
+links = []
+for a in soup.find('a'):
+    try:
+        link = a.attrs['href']
+        links.append(link)
+    except AttributeError:
+        pass
+
+links = [l for l in links if 'wikipedia.org' in l]
+
+print(links)
---
+++ scrape/titans.py
@@ -0,0 +1,10 @@
+from gazpacho import get, Soup
+
+url = 'http://www.scrape.world/titans'
+
+html = get(url)
+soup = Soup(html)
+
+titans = soup.find('b')[1:]
+titans = [t.find('a').text for t in titans]
+print(titans)
