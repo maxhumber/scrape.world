55bb6a4d3d7acea154de5c7f4e34dc4be1534c9f
Max Humber <max.humber@gmail.com>
2020-02-17 20:44:59 -0500

    fish

---
+++ solutions/fish.py
@@ -0,0 +1,31 @@
+import re
+from textwrap import indent
+from urllib.request import urlopen
+from gazpacho import get, Soup
+from PIL import Image # $ pip install pillow
+import pytesseract
+
+base = 'http://scrape.world'
+url = base + '/fish'
+html = get(url)
+soup = Soup(html)
+
+imgs = soup.find('img')[1:]
+paths = [i.attrs['src'] for i in imgs]
+
+images = []
+for path in paths:
+    url = base + path
+    im = Image.open(urlopen(url))
+    images.append(im)
+
+text = ''
+for image in images:
+    i2t = pytesseract.image_to_string(image)
+    text += i2t
+
+pattern = re.compile('(?<![.!?\’\”\'])\n')
+text = re.sub(pattern, ' ', text)
+text = indent(text, '\t')
+
+print(text)
