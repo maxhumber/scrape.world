7bf22c801cdd1390c523af9bf9d9b633a1ecb707
Max Humber <max.humber@gmail.com>
2020-02-17 19:56:23 -0500

    season.py

--- challenges/spend.py
+++ challenges/spend.py
@@ -6,6 +6,7 @@ soup = Soup(html)
 
 trs = soup.find("tr", {"class": "tmx"})
 
+
 def parse_tr(tr):
     team = tr.find("td", {"data-label": "TEAM"}).text
     spend = float(
@@ -13,19 +14,7 @@ def parse_tr(tr):
     )
     return team, spend
 
+
 spend = [parse_tr(tr) for tr in trs]
 
 print(spend)
-
-url = "http://www.scrape.world/season"
-browser.get(url)
-
-tables = pd.read_html(browser.page_source)
-east = tables[0]
-west = tables[1]
-df = pd.concat([east, west], axis=0)
-df["W"] = df["W"].apply(pd.to_numeric, errors="coerce")
-df = df.dropna(subset=["W"])
-df = df[["Team", "W"]]
-df = df.rename(columns={"Team": "team", "W": "wins"})
-df
