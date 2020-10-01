b9d0a482a991736c001861f36bc81c0fe4f8a141
archit ghetiya <30925578+archit00007@users.noreply.github.com>
2020-10-01 11:12:00 +0530

    update app.py #116 app run on port

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def loaderio():
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("APP RUN ON PORT", 5000))
     app.run(host="0.0.0.0", port=port)
